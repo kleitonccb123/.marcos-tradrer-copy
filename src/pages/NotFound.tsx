857de99e18c723b25d48c231dd2672f36ba4e6df
kleitonccb123 <sejadigitalmarketclientes@gmail.com>
2025-10-02 17:01:57 -0300

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+// src/pages/NotFound.tsx
+export default function NotFound() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>404 — Página não encontrada</h1>
+      <p>Ops! O conteúdo que você procura não existe.</p>
+    </main>
+  );
+}
